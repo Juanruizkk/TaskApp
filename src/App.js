69f7588ae504509bdc2a92b6367283cfd9a0618f
Juan Ruiz <juanignacioruizr@gmail.com>
2025-04-10 09:53:58 -0300

    Agregamos toggle para mostrar tareas completadas

--- src/App.js
+++ src/App.js
@@ -1,43 +1,59 @@
-import { useState,useEffect } from "react";
-import {TaskCreator} from "./components/TaskCreator";
+import { useState, useEffect } from "react";
+import { TaskCreator } from "./components/TaskCreator";
 import "./App.css";
 import { TaskTable } from "./components/TaskTable";
 
 function App() {
-  
-  const [taskItems,setTaskItems] = useState([
-  
-  ]);
+  const [taskItems, setTaskItems] = useState([]);
+  // Estado para mostrar las tareas completadas
+  const [showCompleted, setShowCompleted] = useState(false);
   // Funcion para crear una nueva tarea,  es llamada cada vez que envio el formulario en el componente TaskCreator
-  function createNewTask(TaskName){
+  function createNewTask(TaskName) {
     if (!taskItems.find((task) => task.name === TaskName)) {
-      setTaskItems([...taskItems, {name: TaskName, done: false}]);
+      setTaskItems([...taskItems, { name: TaskName, done: false }]);
     }
-   
   }
 
-const toggleTask = (taskName) => {
-  setTaskItems(taskItems.map(t => (t.name == taskName) ? {...t, done: !t.done} : t));
-
-}
+  const toggleTask = (taskName) => {
+    setTaskItems(
+      taskItems.map((t) => (t.name == taskName ? { ...t, done: !t.done } : t))
+    );
+  };
 
   // Listo las tareas desde el localStorage al cargar la aplicacion
   useEffect(() => {
     let datos = localStorage.getItem("tasks");
-    if(datos){
+    if (datos) {
       setTaskItems(JSON.parse(datos));
     }
-  },[])
+  }, []);
   // Guardar las tareas en el localStorage cada vez que cambian
   // el estado de las tareas
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(taskItems));
-  }, [taskItems])
+  }, [taskItems]);
 
   return (
     <div className="App">
-        <TaskCreator createNewTask={createNewTask}/>
-       <TaskTable taskItems={taskItems} toggleTask={toggleTask}/>
+      <TaskCreator createNewTask={createNewTask} />
+      <TaskTable taskItems={taskItems} toggleTask={toggleTask} />
+
+      <div>
+        <input
+          type="checkbox"
+          onChange={(e) => {
+            setShowCompleted(!showCompleted);
+          }}
+        />
+        <label>Show Tasks Done</label>
+      </div>
+      {showCompleted && (
+        <TaskTable
+          taskItems={taskItems}
+          toggleTask={toggleTask}
+          showCompleted={showCompleted}
+        />
+      )}
     </div>
   );
 }
--- src/components/TaskTable.js
+++ src/components/TaskTable.js
@@ -1,18 +1,28 @@
-import { TaskRow } from "./TaskRow"
-export const TaskTable =({taskItems, toggleTask}) => {
-    return (
+import { TaskRow } from "./TaskRow";
+export const TaskTable = ({ taskItems, toggleTask, showCompleted = false }) => {
+  const taskTableRow = (doneValue) => {
+    // Filtrar las tareas por el estado de la tarea
+    return taskItems
+      .filter((task) => task.done === doneValue)
+      .map((task) => (
+        <TaskRow
+          task={task}
+          key={task.name}
+          toggleTask={toggleTask}
+          showCompleted
+        />
+      ));
+  };
+
+  return (
     <table>
-    <thead>
-      <tr>
-        <th>Tarea</th>
-        <th>Estado</th>
-      </tr>
-    </thead>
-    <tbody>
-      {taskItems.map((task) => (
-        <TaskRow task={task} key={task.name} toggleTask={toggleTask}/>
-      ))}
-    </tbody>
-  </table>
-  )
-}
+      <thead>
+        <tr>
+          <th>Tarea</th>
+          <th>Estado</th>
+        </tr>
+      </thead>
+      <tbody>{taskTableRow(showCompleted)}</tbody>
+    </table>
+  );
+};
